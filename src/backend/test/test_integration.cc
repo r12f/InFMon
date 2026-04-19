f6bfa148f5d917a760df870c97fb41f8ae52253d
Riff <r12f.code@gmail.com>
2026-04-19 19:14:39 +0000

    fix: apply clang-format to test_integration.cc (include order, brace style)
    Signed-off-by: r12f <r12f.code@gmail.com>

--- src/backend/test/test_integration.cc
+++ src/backend/test/test_integration.cc
@@ -4,13 +4,14 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include "infmon/graph_node.h"
-#include "infmon/flow_rule.h"
 #include "infmon/counter_table.h"
+#include "infmon/flow_rule.h"
+#include "infmon/graph_node.h"
 }
 
 // Placeholder: verify that the integration test target links successfully
 // against all backend libraries.
-TEST(Integration, LinksSuccessfully) {
+TEST(Integration, LinksSuccessfully)
+{
     SUCCEED();
 }
